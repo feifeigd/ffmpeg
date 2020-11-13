8a0b156a4c656a1d62a3280c8974d85a2a44a581
feifeigd <feifeigd@21cn.com>
2020-11-13 20:13:27 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <libavdevice/avdevice.h>
+#include <libavcodec/avcodec.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/time.h>
+#include <libavutil/audio_fifo.h>
+#include <libavutil/avutil.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+
+int main(){
+	return 0;
+}
+
